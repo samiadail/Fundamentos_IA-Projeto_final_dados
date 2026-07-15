88e3f01ac7c5f88490da7c472349242a1ea11673
samiadail <samiadail12@gmail.com>
2026-07-15 14:54:30 -0300

    update: alteração da saida

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def prever():
         
         preco = modelo.predict(carro)[0]
         return jsonify({
-            "preço":round(float(preco), 2)
+            "preço teste":round(float(preco), 2)
         })
 
 
